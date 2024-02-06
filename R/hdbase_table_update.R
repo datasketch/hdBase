82c500597720f1b0ae813930649b56505a0fe2a9
jpmarindiaz <jpmarindiaz@gmail.com>
2024-02-06 08:04:42 -0500

    added hdbase_table_update and delete

--- R/hdbase_table_update.R
+++ R/hdbase_table_update.R
@@ -1,11 +1,12 @@
 
-
+#' @export
 hdbase_table_update <- function(hb, table_slug = NULL, table = NULL, ...){
   if(is.null(table)) stop("Needs a data.frame or hdtable to updata hdbase")
   hb$hdtables[[table_slug]] <- hdtable(table, slug = table_slug, ...)
   hb
 }
 
+#' @export
 hdbase_table_delete <- function(hb, table_slug = NULL){
   hb$hdtables[[table_slug]] <- NULL
   hb
